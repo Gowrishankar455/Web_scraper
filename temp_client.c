eb9dc036dba933753951af0ff88916a2a89ae793
Gowrishankar455 <80194810+Gowrishankar455@users.noreply.github.com>
2022-02-21 17:03:44 +0530

    Add files via upload

---
+++ temp_client.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<errno.h>
+#include<string.h>
+#include<netdb.h>
+int main(int argc,char* argv[])
+{       char domain[100];
+	char path[100];
+        strcpy(domain,argv[1]);
+	strcpy(path,argv[2]);
+
+        if(argc<2){
+		printf("Invalid input");
+	}
+        printf("argc:%d,domain: %s,path:%s\n",argc,domain,path);
+	struct hostent *he;
+	he = gethostbyname(domain);
+	if (he == NULL){
+       herror("gethostbyname");
+       exit(1);
+        }
+
+        int client_socket=socket(AF_INET,SOCK_STREAM,0);
+        if(client_socket==-1){
+		perror("Socket");
+                exit(1);
+	}
+        struct sockaddr_in remote_address;
+	remote_address.sin_family=AF_INET;
+	remote_address.sin_port=htons(80);
+	remote_address.sin_addr = *((struct in_addr *)he->h_addr);
+        bzero(&(remote_address.sin_zero),8); 
+
+
+	if (connect(client_socket, (struct sockaddr *)&remote_address,sizeof(struct sockaddr)) == -1){
+       perror("Connect");
+       exit(1);
+         }
+
+	char request[1024];
+	char response[9999];
+        snprintf(request, sizeof(request), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", path, domain);
+        printf("%s",request);
+        if(send(client_socket, request, strlen(request), 0)==-1){
+        perror("send");
+        exit(1); 
+        }
+    printf("Data sent.\n");  
+    int bytes_received;
+    char *p;
+    FILE *fp=fopen("received_file.txt","wb");
+    printf("Recieving data...\n\n");
+   
+   int bytes=0;
+   char *ptr=response+1;
+    while(bytes_received = recv(client_socket, ptr, 1, 0)){
+        if(bytes_received==-1){
+            perror("Parse Header");
+            exit(1);
+        }
+        if(
+            (ptr[-3]=='\r')  && (ptr[-2]=='\n' ) &&
+            (ptr[-1]=='\r')  && (*ptr=='\n' )
+        ) break;
+
+        ptr++;
+    }
+
+
+   while( bytes_received = recv(client_socket, response, sizeof(response), 0)){
+    if( bytes_received== -1 )
+	{
+		perror("receive");
+		return(1);
+	}
+    bytes+=bytes_received;
+    fwrite(response,1,bytes_received,fp);
+     printf("Bytes recieved: %d\n",bytes);
+    if(bytes==-1)
+	    break;
+   }
+    printf("Successfully received\n");
+    close(client_socket);
+	fclose(fp);
+	return 0;
+}
