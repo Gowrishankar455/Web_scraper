b9e78b88fa865c6a3a6e3b026d4d71d81118354f
Gowrishankar455 <80194810+Gowrishankar455@users.noreply.github.com>
2022-03-01 19:03:02 +0530

    Update html_parser.c

--- html_parser.c
+++ html_parser.c
@@ -4,14 +4,14 @@
 #include<string.h>
 
 int scripstyle(FILE* ptr){
-char c[10],ch;
+char c[10],ch=' ';
 int i=0;
 while(1)
 { 
-if(ch=='>')   
-{
-	c[i-1]='\0';
-        if(strcmp("script",c)==0||strcmp("style",c)==0)   //finding close tag for style and script commands
+if(ch=='>')
+{	if(i!=0)
+       c[i-1]='\0';
+        if(strcmp("script",c)==0||strcmp("style",c)==0)
 		return 1;
 	else 
 		return 0;
@@ -22,44 +22,42 @@ if(ch=='>')
 return 0;
 }
 
-
 void findword(FILE* ptr,int k){
  int i=0;
  char c[100],ch;
  int afl=0;
  c[i++]=k;
   while(1){
-		  if((ch=fgetc(ptr))=='>'){     //Search for close tag
+		  if((ch=fgetc(ptr))=='>'){
 		     c[i]='\0';
-		     int flag=0; 
-		
-                     if(strcmp("script",c)==0||strcmp("style",c)==0){   //for script and style commands
+		     int flag=0; 	
+                     if(strcmp("script",c)==0||strcmp("style",c)==0){
 			     while(1){
 				  ch=fgetc(ptr);
-		                if(ch=='<'&&(ch=fgetc(ptr))=='/')
+				  putchar(ch);
+				 if(ch=='<'&&(ch=fgetc(ptr))=='/')
 		                if(scripstyle(ptr))
 				       return ;	
 			     }
 		     }
-		  return;
-		  }
+		  return;		  }
         if(ch==' '||ch=='\n') afl=1;
 	if(!afl)
 	  c[i++]=ch;
-
+	// putchar(ch);
   }
 }
-
 void fn1(){
 FILE* ptr=fopen("received_file.txt","r");
 FILE* fp=fopen("Words.txt","w");
 int ch;
 int flag=0;
+int linenum=1;
  do{
 	 ch=fgetc(ptr);
 	 if(ch=='<'){
             ch=fgetc(ptr);
-	    if(ch=='!')     //Skip the comments
+	    if(ch=='!')
 	    {
 		    while(1){
                        if((ch=fgetc(ptr))=='-'||ch=='D')
@@ -77,28 +75,32 @@ int flag=0;
 		 findword(ptr,ch);
 	 }
 	 else{
-		   if(ch=='\n'&&!flag)
+		        if(ch=='\n'&&!flag)
 		 	 fputc(ch,fp);
 			  else if(ch!='\n'){
 			  fputc(ch,fp);
 			  flag=0;
-	 }
-	 else
-	 continue;
+	                    }
+	                 else{
+	                  flag=1;
+	                  continue;
+	                   }
 	 }
  }while(ch!=EOF);
- 
 fclose(fp);
 fclose(ptr);
 FILE *fpt=fopen("Words.txt","r");
 FILE *ptrr=fopen("Revised_word.txt","w");
 int chr;
 
-do                            // Removing the special characters and extra spaces from the parsed file
+do 
 {
 chr=fgetc(fpt);
-if(chr>=32&&ch<=126)
+if(chr>=32&&chr<=126){
+if(chr=='.'||chr==','||chr=='('||chr=='['||chr=='{'||chr==')'||chr==']'||chr=='}'||chr==':'||chr==';')
+chr='\n';
 fputc(chr,ptrr);
+}
 if(chr=='\n')
 fputc(' ',ptrr);
 if(chr==EOF)
@@ -108,4 +110,5 @@ fputc(chr,ptrr);
 fclose(fpt);
 fclose(ptrr);
 }
+
 
