a1a9b791816e8aae415065b03bf644e45b1d9d67
Gowrishankar455 <80194810+Gowrishankar455@users.noreply.github.com>
2022-03-09 18:32:07 +0530

    Add files via upload

---
+++ SSL_html_parser.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<string.h>
+
+int scripstyle(FILE* ptr){
+char c[10],ch;
+int i=0;
+while(1)
+{ 
+ ch=fgetc(ptr);
+if(ch=='>')
+{       c[i]='\0';
+//printf("|| %s at\n",c);
+        if(strcmp("script",c)==0||strcmp("style",c)==0)
+		return 1;
+	else 
+		return 0;
+}
+ c[i++]=ch;
+}
+return 0;
+}
+
+void findword(FILE* ptr,int k){
+ int i=0;
+ char c[100],ch;
+ int afl=0;
+ c[i++]=k;
+  while(1){
+		  if((ch=fgetc(ptr))=='>'){
+		     c[i]='\0';
+		//     printf("|| %s in\n",c);
+		     int flag=0; 	
+                     if(strcmp("script",c)==0||strcmp("style",c)==0){
+			     while(1){
+				  ch=fgetc(ptr);
+				 if(ch=='<'&&(ch=fgetc(ptr))=='/')
+		                if(scripstyle(ptr))
+				       return ;	
+			     }
+		     }
+		  return;		  }
+        if(ch==' '||ch=='\n') afl=1;
+	if(!afl)
+	  c[i++]=ch;
+	// putchar(ch);
+  }
+}
+void fn1(){
+FILE* ptr=fopen("SSL_Rec_file.txt","r");
+FILE* fp=fopen("SSL_Words.txt","w");
+int ch;
+int flag=0;
+int linenum=1;
+ do{
+	 ch=fgetc(ptr);
+	 if(ch=='<'){
+            ch=fgetc(ptr);
+	    if(ch=='!')
+	    {
+		    while(1){
+                       if((ch=fgetc(ptr))=='-'||ch=='D')
+			       if((ch=fgetc(ptr))=='-'||ch=='O')
+				       if((ch=fgetc(ptr))=='>'||ch=='C')
+					       break;
+		    }
+			if(ch=='>')
+			continue;
+			else{
+				while(ch!='>') ch=fgetc(ptr);
+			}
+	    }
+	    else
+		 findword(ptr,ch);
+	 }
+	 else{
+		        if(ch=='\n'&&!flag)
+		 	 fputc(ch,fp);
+			  else if(ch!='\n'){
+			  fputc(ch,fp);
+			  flag=0;
+	                    }
+	                 else{
+	                  flag=1;
+	                  continue;
+	                   }
+	 }
+ }while(ch!=EOF);
+fclose(fp);
+fclose(ptr);
+FILE *fpt=fopen("SSL_Words.txt","r");
+FILE *ptrr=fopen("SSL_Words_revised.txt","w");
+int chr;
+
+do 
+{
+chr=fgetc(fpt);
+if(chr>=32&&chr<=126){
+if(chr=='.'||chr==','||chr=='('||chr=='['||chr=='{'||chr==')'||chr==']'||chr=='}'||chr==':'||chr==';'||chr=='\"')
+chr='\n';
+fputc(chr,ptrr);
+}
+if(chr=='\n')
+fputc(' ',ptrr);
+if(chr==EOF)
+fputc(chr,ptrr);
+}while((chr!=EOF));
+
+fclose(fpt);
+fclose(ptrr);
+}
+
