6838b61b69c7f04229812867997157140bc70dbc
Gowrishankar455 <80194810+Gowrishankar455@users.noreply.github.com>
2022-03-11 19:42:23 +0530

    Add files via upload

---
+++ HTTPS/SSL_word_tree.c
@@ -0,0 +1,135 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+//Struct tree to store word and frequency
+    struct tree
+    {
+	  char *word;
+	  int count;
+	  struct tree* left;
+	  struct tree* right;
+    };
+   
+//Function to convert Uppercase to Lowercase
+   void kstrlwr(char ch[],int i) 
+   {
+          for(int k=0;k<i;k++)
+          if(ch[k]>='A'&&ch[k]<='Z')
+          ch[k]=ch[k]+32;
+   }
+
+//Function to allocate memory for the word
+   char *mstrdup(char *s)       
+   {
+          char *p;
+          p = (char *) malloc(strlen(s) + 1);
+          if(p != NULL)
+             strcpy(p, s);
+          return p;
+   }
+
+//Function to create a word in the tree
+   struct tree* talloc(char* ch)  
+   {
+          struct tree* temp=(struct tree*)malloc(sizeof(struct tree));
+	  temp->word=mstrdup(ch);
+	  temp->count=1;
+	  temp->left=NULL;
+	  temp->right=NULL;
+	  return temp;	
+   }
+   
+//Function to add a word to tree
+   struct tree*  addword(char* ch,struct tree* root)  
+   {
+         int cmp;
+         if(root==NULL)
+         {
+	     root= talloc(ch);
+	     return root;
+         }
+         else if((cmp=strcmp(ch,root->word))<0)
+	     root->left=addword(ch,root->left);
+         else if(cmp>0)
+	     root->right=addword(ch,root->right);
+         else if(cmp==0)
+          {
+	     root->count++;
+          }
+         return root;
+   }
+
+//Functions to rank the words based on their frequency
+   void rank(int t,struct tree* root,FILE* fp)   
+   {
+       if(root==NULL)
+          return ;
+       rank(t,root->left,fp);
+       if(t==root->count)
+       {
+           fputs(root->word,fp);
+           fputc(' ',fp);
+	   fprintf(fp,"%d",t);
+	   fputc('\n',fp);
+       }
+       rank(t,root->right,fp);
+   }
+
+//Function to find the maximum frequency
+   void findmax(int *max,struct tree* root)   
+   {
+        if(root==NULL)
+	return;
+        findmax(max,root->left);
+        if(root->count>*max)
+        {
+        *max=root->count;
+        } 
+        findmax(max,root->right);
+   }
+
+//Function to rank words based on their frequency
+   void ranker()                           
+   {
+       struct tree* root=NULL;
+       FILE *fp=fopen("SSL_Words_revised.txt","r");
+       if(fp==NULL)
+         printf("No file exists");
+       int c;
+       char ch[30];
+       int i=0;
+       while((c=fgetc(fp))!=EOF)
+       {
+         if(c!='\n'&&c!=' ')
+	     ch[i++]=c;
+         else if(c=='\n'&&i==0)
+            continue;
+         else if(c==' '&&i==0)
+            continue;
+         else
+         {
+            ch[i]='\0';
+            if(strlen(ch)==1||strlen(ch)>30)
+            {
+               i=0;
+               continue;
+            }     
+            kstrlwr(ch,i);
+            root=addword(ch,root);
+            i=0;
+         }
+      }
+
+      fclose(fp);
+      int max=0;
+      findmax(&max,root);
+      fp=fopen("SSL_Result.txt","w");
+      for(int t=max;t>0;t--)
+      {
+	 rank(t,root,fp);
+      }
+      fclose(fp);
+   }
+
---
+++ HTTPS/ssl_client.c
@@ -0,0 +1,195 @@
+#include <sys/socket.h>
+#include <sys/errno.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <openssl/crypto.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include "SSL_html_parser.c"
+#include "SSL_word_tree.c"
+
+#define PORT 443
+
+int main(int argc,char* argv[])
+{    
+       char domain[100];
+       char path[100];
+       if(argc==2)
+        {                       //Urls with only domain name
+		printf("No path\n");
+               strcpy(domain,argv[1]);
+        }
+	if(argc==3)
+	 {                      //Urls with domain name and path
+	       strcpy(domain,argv[1]);
+               strcpy(path,argv[2]);
+	 }
+	
+        int sock;
+        int iResult;            //Declaring a socket descriptor
+        char  *ip;              //Pointer to ip address
+        struct sockaddr_in sin; //Structure to hold Ipv4 address
+        struct hostent* hent;   //Structure to hold internet host
+
+        hent = gethostbyname(domain);  //Converting the domain to ip address
+        if(hent == NULL)
+        {
+              printf("gethostbyname failed: %d\n", errno);  
+              return -1;
+        }
+        printf("gethostbyname succeeded\n");
+       
+        ip = inet_ntoa(*((struct in_addr*)hent->h_addr_list[0]));     //Function to return the ip address in string 
+        printf("Host IP: %s\n", ip);
+    
+        sock = socket(AF_INET, SOCK_STREAM, 0); //Function call to get the socket descriptor
+        if(sock == -1)
+        {
+              printf("socket failed: %d\n", errno);
+              return -1;
+        }
+        printf("socket created\n");
+    
+        sin.sin_family = AF_INET;                // Assigning address family
+        sin.sin_addr.s_addr = inet_addr(ip);     // Assigning Ip address
+        sin.sin_port = htons(PORT);              // Assigning Port number
+         
+        iResult = connect(sock, (struct sockaddr*)&sin, sizeof(sin));  //Function to connect to host     
+        if(iResult < 0)
+        {
+               printf("connect failed: %d\n", errno);
+               return -1;
+        }
+        printf("connect succeeded\n");
+    
+        iResult = SSL_library_init();           //Initialize the available SSL/TLS ciphers and digests
+        if(iResult < 0)
+        {
+               printf("SSL failed\n");
+               return -1;
+        }
+        printf("SSL library initialised\n");
+       
+        OpenSSL_add_all_algorithms();          //Registers the available SSL/TLS ciphers and digests
+        ERR_load_crypto_strings();             //Registers the error strings for all libcrypto functions.
+        SSL_load_error_strings();              //Registers the libssl error strings
+        SSL_CTX* ctx = SSL_CTX_new(TLSv1_2_client_method());         //Function to create a new context for client application
+       
+        if(ctx == NULL)
+        {
+               printf("ctx failed\n");
+               ERR_print_errors_fp(stderr);
+               return -1;
+        }
+        printf("ctx loaded\n");
+        SSL* ssl = SSL_new(ctx);               //Creates SSL structure to hold the data for a TLS/SSL connection.
+        if(ssl == NULL)
+        {
+               printf("ssl failed\n");
+               ERR_print_errors_fp(stderr);
+               return -1;
+        }
+        printf("ssl loaded\n");
+        SSL_set_fd(ssl, sock);                //Function to assign a socket to SSL structure
+        SSL_connect(ssl);                     //Initiates the TLS/SSL handshake with the server
+   
+        char request[1024];
+        char response[9999];
+        
+        //GET request in a request buffer
+        snprintf(request, sizeof(request), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", path, domain);
+          
+        iResult = SSL_write(ssl, request, strlen(request));           //Function to write the request buffer into the ssl connection
+        if(iResult <= 0)
+        {
+              printf("SSL write failed\n");
+              ERR_print_errors_fp(stderr);
+              return -1;
+        }
+        printf("Byte(s) sent: %d\n", iResult);
+        bzero(response, sizeof(response));    //Assigning the receiver buffer to zero
+         
+        char *p;
+        FILE *fp=fopen("SSL_Rec_file.txt","wb");                     //Creating a file for saving response
+        printf("Recieving data...\n\n");
+   
+        int bytes=0;
+        char *ptr=response+1;
+ 
+ //skip the status and other information
+        while(iResult = SSL_read(ssl, ptr,1))
+        {
+              if(iResult==-1)
+              {
+                  perror("Parse Header");
+                  exit(1);
+              }
+             if(
+             (ptr[-3]=='\r')  && (ptr[-2]=='\n' ) &&
+             (ptr[-1]=='\r')  && (*ptr=='\n' )
+             )    break;
+
+             ptr++;
+        }
+ 
+ //Saving html source code
+
+       while( iResult = SSL_read(ssl, response,sizeof(response)))
+       {
+             if(iResult== -1 )
+	      {
+	     	  perror("receive");
+	 	  return(1);
+	      } 
+	     if(iResult<0)
+	          break;
+             bytes+=iResult;
+             fwrite(response,1,iResult,fp);
+             printf("Bytes recieved: %d\n",bytes);
+
+       }
+       printf("\nSuccessfully received\n");
+       fclose(fp);
+    
+       iResult = SSL_shutdown(ssl);     //Shutdowns the SSL connection
+       if(iResult == 0)
+       {
+               printf("SSL shutdown in progress...\n");
+       }
+       iResult = SSL_shutdown(ssl);
+       if(iResult == 1)
+        {
+               printf("SSL shutdown complete!\n");
+        }
+       if(iResult == -1)
+        {
+               printf("SSL shutdown unsuccessful!\n");
+        }
+        SSL_CTX_free(ctx);             //Removes the SSL_CTX object pointed to by ctx and frees up the memory
+    
+        iResult = shutdown(sock, SHUT_RDWR);
+        if(iResult == -1)
+        {
+               printf("Socket shutdown failed: %d\n", errno);
+               return -1;
+        }
+        printf("Socket shutdown succeeded\n");
+        iResult = close(sock);        //Shutdowns the socket connection associated with socket descriptor
+        if(iResult != 0)
+        {
+               printf("error closing socket: %d\n", errno);
+               return -1;
+        }
+        printf("Socket closed\n");
+        parser();                      //Function to parse the html source code
+	printf("Successfully parsed\n");
+	ranker();                    //Function to rank the words
+	printf("Successfully ranked\n");
+	return 0;
+}
